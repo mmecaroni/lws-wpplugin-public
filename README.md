95bddc66820423cee32bc10f7067b2fddd6d969c
Mario Mecaroni <mmecaroni@gmail.com>
2024-02-23 22:33:02 -0700

    v0.0.2-mario

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# lws-wpplugin-public v0.0.0
+# lws-wpplugin-public v0.0.2
 
 Just a testing public plugin to see how to update it from github
--- lws-wpplugin-public.php
+++ lws-wpplugin-public.php
@@ -3,7 +3,7 @@
 Plugin Name: LWS WpPlugin Public
 Plugin URI: http://www.liquidstudiodev.com/wp/lws-wpplugin-public
 Description: Just a testing public plugin to see how to update it from github
-Version: 0.0.1
+Version: 0.0.2
 Author: Mario Mecaroni
 Author URI: http://www.liquidstudiogroup.com/mario-mecaroni
 License: MIT
@@ -12,28 +12,15 @@ GitHub Plugin URI: https://github.com/mmecaroni/lws-wpplugin-public
 GitHub Branch: master
 */
 
+/****** Get lost! */
 if (!defined('ABSPATH')) { exit; }
 /****** Display Version */
-echo "v0.0.1";
+add_action("wp_footer", function() {
+	echo "LWS WPPlugin Public Version: 0.0.2";
+});
 /****** Constant for Plugin Path */
 define('LWS_WPPLUGIN_PUBLIC_FILE_PATH', plugin_dir_path(__FILE__));
 /****** Main Plugin File Constant */
 define('LWS_WPPLUGIN_PUBLIC_MAIN_FILE', __FILE__);
 /****** EXAMPLE REQUIRE */
 // require_once trailingslashit(LWS_WPPLUGIN_PUBLIC_FILE_PATH) . "packages/dashboard/page-dashboard.php";
-
-/**
- * Plugin Updater Script
- * Must be in this main file.
- */
-// require 'path/to/plugin-update-checker/plugin-update-checker.php';
-require_once trailingslashit(LWS_WPPLUGIN_PUBLIC_FILE_PATH) . "plugin-update-checker/plugin-update-checker.php";
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/mmecaroni/lws-wpplugin-public',
-	__FILE__, // Full path to the main plugin file or functions.php.
-	'lws-wpplugin-public'
-);
-// Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('master');
-//Optional: If you're using a private repository, specify the access token like this:
-// $myUpdateChecker->setAuthentication('your-token-here');
